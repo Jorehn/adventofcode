2d60683adfe7985f5577e5d90bfb034b226ae312
John Renne <john@MacBook-Pro.localdomain>
2020-12-01 14:47:35 +0100

    second challenge

--- 2020/day1/01012020.py
+++ 2020/day1/01012020.py
@@ -8,11 +8,18 @@ def read_integers(filename):
     
     
 my_list = read_integers("list.txt")
+
+def need_value(integer_list, needed_value):
+    for found_int in integer_list:
+        need_int = needed_value - found_int
+        if need_int in integer_list:
+            return [need_int,found_int]
+
+print(need_value(my_list,2020)[0]*need_value(my_list,2020)[1])
 for found_int in my_list:
-    need_int = 2020 - found_int
-    if need_int in my_list:
-        result = need_int * found_int
-        print(result)
+    needed_value = 2020 - found_int
+    if need_value(my_list, needed_value):
+        print(found_int*need_value(my_list, needed_value)[0]*need_value(my_list, needed_value)[1])
         break
     
 
